af4c49150bfd1dd7ab33657bd3fb44782850ef2e
Jinwon-Dev <yjw9424@naver.com>
2023-05-04 01:11:14 +0900

    코딩 테스트 입문 : 두 수의 나눗셈

---
+++ src/pg_120806/Solution.java
@@ -0,0 +1,12 @@
+package pg_120806;
+
+public class Solution {
+
+    public int solution(int num1, int num2) {
+
+        int answer = 0;
+
+        double answers = (double) num1 / num2 * 1000;
+        return answer = (int) answers;
+    }
+}
