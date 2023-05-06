e6032f0ecaa9cc3a6c4c9238a67818b4d6b23c4b
Jinwon-Dev <yjw9424@naver.com>
2023-05-06 20:10:27 +0900

    코딩 테스트 입문 : 문자열 뒤집기

---
+++ src/pg_120822/Solution.java
@@ -0,0 +1,15 @@
+package pg_120822;
+
+public class Solution {
+
+    public String solution(String my_string) {
+
+        String answer = "";
+
+        StringBuilder sb = new StringBuilder(my_string);
+
+        answer = sb.reverse().toString();
+
+        return answer;
+    }
+}
