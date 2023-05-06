006d64ba3246cf8822fcf142a89cca0ce56c8cd7
Jinwon-Dev <yjw9424@naver.com>
2023-05-06 20:03:59 +0900

    코딩 테스트 입문 : 편지

---
+++ src/pg_120898/Solution.java
@@ -0,0 +1,13 @@
+package pg_120898;
+
+public class Solution {
+
+    public int solution(String message) {
+
+        int answer = 0;
+
+        answer = message.length() * 2;
+
+        return answer;
+    }
+}
