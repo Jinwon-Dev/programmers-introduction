c73e698a5625f98fef9a664b45c708990ee0d092
Jinwon-Dev <yjw9424@naver.com>
2023-05-10 13:11:38 +0900

    코딩 테스트 입문 : 대문자와 소문자

---
+++ src/pg_120893/Solution.java
@@ -0,0 +1,19 @@
+package pg_120893;
+
+public class Solution {
+
+    public String solution(String my_string) {
+
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isLowerCase(my_string.charAt(i))) {
+                answer += Character.toUpperCase(my_string.charAt(i));
+            } else {
+                answer += Character.toLowerCase(my_string.charAt(i));
+            }
+        }
+
+        return answer;
+    }
+}
