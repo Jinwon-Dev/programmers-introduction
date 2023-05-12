f1b652d46bd6e5e859536d6d7e46cc79de44853c
Jinwon-Dev <yjw9424@naver.com>
2023-05-12 23:20:09 +0900

    코딩 테스트 입문 : 문자열 정렬하기(2)

---
+++ src/pg_120911/Solution.java
@@ -0,0 +1,21 @@
+package pg_120911;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public String solution(String my_string) {
+
+        String answer = "";
+
+        my_string = my_string.toLowerCase();
+
+        char[] arr = my_string.toCharArray();
+
+        Arrays.sort(arr);
+
+        answer = new String(arr);
+
+        return answer;
+    }
+}
