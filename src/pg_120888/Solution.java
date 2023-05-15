f9fdd5b00f63ded7fb77cdbf9e4b5292ecd4d227
Jinwon-Dev <yjw9424@naver.com>
2023-05-15 14:16:26 +0900

    코딩 테스트 입문 : 중복된 문자 제거

---
+++ src/pg_120888/Solution.java
@@ -0,0 +1,17 @@
+package pg_120888;
+
+public class Solution {
+
+    public String solution(String my_string) {
+
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (!answer.contains(String.valueOf(my_string.charAt(i)))) {
+                answer += my_string.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
