b55c98007f33b13684167a98ad0b5a25871840d4
Jinwon-Dev <yjw9424@naver.com>
2023-05-27 22:35:06 +0900

    코딩 테스트 입문 : 구슬을 나누는 경우의 수

---
+++ src/pg_120840/Solution.java
@@ -0,0 +1,13 @@
+package pg_120840;
+
+public class Solution {
+
+    public int solution(int balls, int share) {
+        return combination(balls, share);
+    }
+
+    public int combination(int balls, int share) {
+        if (balls == share || share == 0) return 1;
+        return combination(balls - 1, share - 1) + combination(balls - 1, share);
+    }
+}
