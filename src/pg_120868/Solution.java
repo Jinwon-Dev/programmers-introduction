b79898a0b515c1ecca486fa63450d0e0f4f73147
Jinwon-Dev <yjw9424@naver.com>
2023-05-30 20:52:37 +0900

    코딩 테스트 입문 : 삼각형의 완성조건(2)

---
+++ src/pg_120868/Solution.java
@@ -0,0 +1,19 @@
+package pg_120868;
+
+public class Solution {
+
+    public int solution(int[] sides) {
+
+        int answer = 0;
+
+        int big = Math.max(sides[0], sides[1]);
+        int small = Math.min(sides[0], sides[1]);
+
+        int low = big - small;
+        int high = big + small;
+
+        answer = high - low - 1;
+
+        return answer;
+    }
+}
